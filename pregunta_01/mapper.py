c47172cbcaf9ab59fe5238c93dddd9ae0c49d1bc
JuanGalindz <jcgalindezr@unal.edu.co>
2023-06-13 20:58:36 -0500

    sol_p1

--- pregunta_01/mapper.py
+++ pregunta_01/mapper.py
@@ -1,3 +1,14 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+#! /usr/bin/ python3
+
+#
+# Esta es la funcion que mapea la entrada a parejas (clave, valor)
+#
+import sys
+if __name__ == "__main__":
+    for line in sys.stdin:
+        line = line.strip()
+        splits = line.split(',')
+        print(splits[2] + '\t' + '1')
